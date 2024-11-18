07f26318cf133e002ea3a5939f6cc84dec1cc41f
Ivan Galushko <galushko.ivan8@gmail.com>
2024-11-18 15:25:08 +0300

    Update template

--- src/main/java/io/github/drednote/template/Application.java
+++ src/main/java/io/github/drednote/template/Application.java
@@ -1,13 +1,13 @@
-package com.github.drednote.template;
+package io.github.drednote.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(TemplateApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
--- src/test/java/io/github/drednote/template/ApplicationTests.java
+++ src/test/java/io/github/drednote/template/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.github.drednote.template;
+package io.github.drednote.template;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TemplateApplicationTests {
+class ApplicationTests {
 
   @Test
   void contextLoads() {
